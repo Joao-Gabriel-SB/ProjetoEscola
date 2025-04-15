150bef3e1636f14d1a769c3ffffb7c72cb94bd47
Joao-Gabriel-SB <joao_gabriel_bispo@hotmail.com>
2025-04-14 22:06:07 -0300

    Add files via upload

--- projeto escola/main.c
+++ projeto escola/main.c
@@ -8,49 +8,58 @@ int main(int argc,char *argv[]){
 
 	setlocale(LC_ALL,"Portuguese");
 
-	char sair=0,key;
+	short int sair=1,key;
 
 	Clear();
-	printf("%s\n\tSeja bem vindo ao sistema de cadastro da escola!\t\t\n %s",bold,resetBold);
+	printf("%s\n\t\tSeja bem vindo ao sistema de cadastro da escola!\t\t\n %s",bold,resetBold);
 
 	do{
-		printf("\n\t\t\t%sMENU INICIAL\n\n\t\t Oque você deseja fazer?%s\n\nMenu cadastro\t(1)\tMenu aluno\t(2)\tMenu Professor\t(3)\nMenu disciplina\t(4)\t%sSair\t(0)%s",bold,reset,yellow_F,reset);
-		campoResposta(&key);
-		Clear();
+		printf("\n\t\t\t\t %sMENU INICIAL\n\n\t\t\t   Oque você deseja fazer?%s\n\n[1] Menu cadastro\t\t[2] Menu aluno\t\t[3] Menu Professor\t\n[4] Menu disciplina\t\t%s[0] Sair\t%s",bold,reset,yellow_F,reset);
+		campoResposta(&key); if(key == 0) sair=0;
+		system("clear||cls");
 
 		switch (key){
 
-		case '1':
-			printf("\t\t%sMENU CADASTRO%s\n\n",bold,reset);
-			printf("\t%sO que você deseja cadastrar:%s\nAluno\t(1)\tProfessor\t(2)\tDisciplina\t(3)",bold,reset);
-			menuIniciar();
+		case 1:
+			printf("\t\t\t%sMENU CADASTRO%s\n\n",bold,reset);
+			printf("[1] Cadastrar\t\t[2] Atualizar\t\t[3] Excluir");
 			campoResposta(&key);
 
-			key = '1';
+				switch(key){
+
+					case 1:
+						printf("\t\t%sO que você deseja cadastrar:%s\n[1] Aluno\t\t[2] Professor\t\t[3] Disciplina\t",bold,reset);
+						menuIniciar();
+						campoResposta(&key);
+						break;
+					case 2:
+						printf("\t\t%sO que você deseja cadastrar:%s\n[1] Aluno\t\t[2] Professor\t\t[3] Disciplina\t",bold,reset);
+						menuIniciar();
+						campoResposta(&key);
+						break;
+					case 3;
+						printf("\t\t%sO que você deseja cadastrar:%s\n[1] Aluno\t\t[2] Professor\t\t[3] Disciplina\t",bold,reset);
+						menuIniciar();
+						campoResposta(&key);
+						break;
+				}
 			break;
-		case '2':
+		case 2:
 			printf("\t\t\t%sMENU ALUNOS%s\n\n",bold,reset);
-			printf("Listar todos os alunos\t(1)\tListar alunos ordenados por...(2)");
+			printf("[1] Listar todos os alunos\t\t[2] Listar alunos ordenados por...");
 			menuIniciar();	campoResposta(&key);	listarAlunos(key);
-
-			key = '1';
 			break;
-		case '3':
+		case 3:
 			printf("\t\t%sMENU PROFESSORES%s\n\n",bold,reset);
-			printf("Listar professores\t(1)");
+			printf("[1] Listar professores\t");
 			menuIniciar();campoResposta(&key);
-
-			key = '1';
 			break;
-		case '4':
+		case 4:
 			printf("\t\t%sMENU DISCIPLINAS%s\n\n",bold,reset);
-			printf("Listar disciplinas\t(2)");
+			printf("[1] Listar disciplinas\t");
 			menuIniciar();campoResposta(&key);
-
-			key = '1';
 			break;
 		}
-	Clear();
-	}while(key != '0');
-
+	system("clear||cls");
+	}while(sair != 0);
 }
