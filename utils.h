683e4d5e117899ea5fcb3e6d4de7d48c51aa1bcc
Joao-Gabriel-SB <joao_gabriel_bispo@hotmail.com>
2025-04-26 17:58:00 -0300

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "utils.c"
+
+void Pause();
+void AnswerField(char *a);
+void Until( char* a, char lowerest, char biggest );
+void StartMenu();
+void Spacer(int tamanhoDaBarra);
+int GetId(void* element);
+int SearchInt( int tamanhoDoVetor, void* Nomevetor, size_t tipoDoElemento, int ( *funcGet )( void* ), int valorDesejado );
+void CharSort( int amountStudents, const int tamVetor1, Student* vetor, Student* vetOrdenado, const int tamVetor2, int selector );
+int SearchChar( Student* vetIn, const int tamVetIn, Student* vetOut, const int tamVetOut, char* strDesejada, int tamstrDesejada );
+int CompareBirthDay( Student a, Student b );
+#endif 
